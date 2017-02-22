ab116f67199631b422c6c69eeda9794996ad28e2
Marc Freeman <freeman.marc880@gmail.com>
2017-02-22 15:54:46 +0000

    Update script.js
    Played around with original script: https://github.com/wesbos/JavaScript30/blob/master/06%20-%20Type%20Ahead/index-FINISHED.html.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// function that confirms if an array is actually array?
+
+var endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+var cities = [];
+
 function to_consumable_array(array) {
     if (!Array.isArray) {
         Array.isArray = function (array) {
@@ -12,38 +15,39 @@ function to_consumable_array(array) {
     }
 }
 
-
-var endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-
-// pushes endpoint (which is fetched from an online source) into a new array, cities.
-var cities = [];
-fetch(endpoint).then(function (blob) {
-    return blob.json();
-}).then(function (data) {
-    return cities.push.apply(cities, to_consumable_array(data));
-});
+function push_array(array) {
+    fetch(endpoint).then(function (blob) {
+        return blob.json();
+    }).then(function (data) {
+        return array.push.apply(array, to_consumable_array(data))
+    })
+}
 
 function findMatches(wordToMatch, cities) {
     return cities.filter(function (place) {
-        // here we need to figure out if the city or state matches what was searched
         var regex = new RegExp(wordToMatch, 'gi');
         return place.city.match(regex) || place.state.match(regex);
     });
 }
 
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+function numberWithCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 function displayMatches() {
     var _this = this;
 
-    var matchArray = findMatches(this.value, cities);
+    var matchArray = findMatches(_this.value, cities);
     var html = matchArray.map(function (place) {
         var regex = new RegExp(_this.value, 'gi');
         var cityName = place.city.replace(regex, '<span class="hl">' + _this.value + '</span>');
         var stateName = place.state.replace(regex, '<span class="hl">' + _this.value + '</span>');
-        return '\n      <li>\n        <span class="name">' + cityName + ', ' + stateName + '</span>\n        <span class="population">' + numberWithCommas(place.population) + '</span>\n      </li>\n    ';
+        return '\n      <li>\n        <span class="name">'
+            + cityName + ', '
+            + stateName
+            + '</span>\n        <span class="population">'
+            + numberWithCommas(place.population)
+            + '</span>\n      </li>\n    ';
     }).join('');
     suggestions.innerHTML = html;
 }
@@ -53,10 +57,13 @@ function select_div_class(html_tag) {
     return document.querySelector(html_tag);
 }
 var searchInput = select_div_class('.search');
-var suggestions = select_div_class('.suggestions');
 
-function add_event_listner(event, match_to_display) {
+var suggestions = select_div_class('.suggestions');
+function add_event_listener(event, match_to_display) {
     return searchInput.addEventListener(event, match_to_display)
 }
-add_event_listner('change', displayMatches);
-add_event_listner('keyup', displayMatches);
+
+push_array(cities);
+
+add_event_listener('change', displayMatches);
+add_event_listener('keyup', displayMatches);
